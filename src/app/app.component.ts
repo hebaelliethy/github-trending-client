3e13c09e49426161c0e1f57ba0406d8592486843
Heba <heba.elliethy1990@gmail.com>
2018-11-16 12:11:46 +0100

    Injecting the service to the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { repos } from './repos';
+import { GithubService } from './trendingrepos.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'github-trending-client';
+export class AppComponent implements OnInit {
+  githubTrending: repos[];
+  totalStars: number = 0;
+  searchTerm: string;
+  constructor(private githubService: GithubService) { };
+
+  ngOnInit() {
+    this.search("");
+  }
+
+  search(searchTerm: string) {
+    console.log(this.searchTerm);
+  
+    this.githubService.getTrending(searchTerm, 10).subscribe(
+      repostrending => {
+
+        this.githubTrending = repostrending.Repositry;
+        this.totalStars = repostrending.SumStars;
+      }
+    )
+  }
+
+  onSearchKeyUp() {
+     this.search(this.searchTerm);
+  }
+
 }
